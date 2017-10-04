9d9e9d0cdcd0efc7e0c318aa7d693d288db60c14
Thomas MICHELET <thomas@mobilispro.com>
2017-10-04 07:47:46 +0200

    [#1] fix issue

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const entrypoints = require('./entrypoints');
+const app = express();
+const jsonwebtoken = require('jsonwebtoken');
+
+const port = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://dev:Rudeboy77@ds161890.mlab.com:61890/affinity');
+
+const authentification = require('./shared/authentification');
+app.use(authentification.initialize());
+
+app.use('/user', entrypoints.user.routes);
+app.use('/api', authentification.authenticate(), entrypoints.place.routes);
+
+app.listen(port);
